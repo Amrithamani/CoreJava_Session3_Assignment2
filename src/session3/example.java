3b979bd7936a5cae53e6bf1a66bece60873046a5
Amrithamani <amrimani91@gmail.com>
2017-05-31 14:55:24 -0400

    committing example program with some folder

---
+++ src/session3/example.java
@@ -0,0 +1,28 @@
+package session3;
+
+public class example {
+
+	// initialize Instance variable x allocated 0 value
+
+	int x;
+
+	// initialize Instance variable y allocated 0 value
+
+	int y;
+
+	// initialize String variable name allocated null value
+
+	String name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		example pnt = new example();// creates an object pnt for the class
+		// example
+
+		System.out.println("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+		// prints pnt is null 0 0
+
+	}
+
+}
